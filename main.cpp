be99ccc8a86b2d115206cfb9589f557ad97e3066
William Campbell <lunarsinged@proton.me>
2023-08-05 14:21:27 -0400

    Initial class and data prototype

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Task {
+	public:
+		string name;
+		int status;
+		string desc;
+		vector<Task> subtasks;
+		Task(string a, int b, string c = "", vector<Task> d = {}) {
+			name = a;
+			status = b;
+			desc = c;
+			subtasks = d;
+		}
+};
+
+int main(int argc, char** argv) {
+	ifstream input;
+	vector<Task> data;
+	string line;
+	input.open(argv[1]);
+	bool sub = false;
+	while (getline(input, line)) {
+		if (line[0] == '#' || line[0] == '(') {
+			continue; // Special commands with (DISPLAY) not added yet
+		} else if (line[0] == '*' || line[0] == '&' || line[0] == '-') {
+			data.push_back(Task(line, 0));
+		}
+	}
+
+	for (unsigned int i = 0; i < data.size(); i++) {
+		cout << data[i].name << endl;
+	}
+	
+}
