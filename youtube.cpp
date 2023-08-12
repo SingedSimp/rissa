aec96ce42454b5ad9c998543f0d5fa3a5ff7e257
William Campbell <lunarsinged@proton.me>
2023-08-11 22:28:54 -0400

    Move ID stuff into functions

--- youtube.cpp
+++ youtube.cpp
@@ -16,34 +16,36 @@ string url2id (string url) {
 	return url;
 }
 
+string getChannelId (string customurl) {
+	// Filter URL with ID from file
+	string query = "(\"https://www.youtube.com/channel/)(.*?)([\"?])";
+	ifstream channel;
+	channel.open("./tmp/@NerissaRavenCroft"); // Temp testing file, will be removed later
+	string line;
+	string id;
+	while (getline(channel, line) && id.empty()) {
+		// Make regex query
+		regex rgx(query);
+		// Hold matches
+		smatch url;
+		// Search file
+		regex_search(line, url, rgx);
+		// Get url id from query
+		if (url[0] != "") {
+			id = url[0];
+			id.erase(0,1);
+			id.erase(id.size() - 1, 1);
+		}
+	}
+	return url2id(id);
+}
+
 int main (int argc, char** argv) {
 	if (argc == 1) {
 		throw std::invalid_argument("No ID Given");
 	} else { // Turn into function later
 		if (argv[1][0] == '@') { // If personalized URL is provided
-			// Filter URL with ID from file
-			string query = "(\"https://www.youtube.com/channel/)(.*?)([\"?])";
-			ifstream channel;
-			channel.open("./tmp/@NerissaRavenCroft"); // Temp testing file, will be removed later
-			string line;
-			string id;
-			while (getline(channel, line) && id.empty()) {
-					// Make regex query
-					regex rgx(query);
-					// Hold matches
-					smatch url;
-					// Search file
-					regex_search(line, url, rgx);
-					// Get url id from query
-					if (url[0] != "") {
-						id = url[0];
-						id.erase(0,1);
-						id.erase(id.size() - 1, 1);
-					}
-			}
-			cout << url2id(id) << endl;
-		} else {
-			cout << "norun" << endl;
+			cout << getChannelId(argv[1]) << endl;
 		}
 	}
 }
